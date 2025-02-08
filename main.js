bb6aac0bbf311dc7ab69130871f785711d7133e8
Deadon <165869639+deadonx@users.noreply.github.com>
2025-02-08 14:31:15 +0530

    added js

---
+++ main.js
@@ -0,0 +1,19 @@
+let names = document.getElementById('text');
+let input = document.getElementById('holder');
+let button = document.getElementById('submit');
+let title = document.getElementById('title');
+function ShowName(valuess) {
+  console.log(input.value);
+ history.pushState({},"",(input.value))
+ title.innerHTML = (input.value);
+  names.innerHTML = (input.value) ;
+}
+
+button.addEventListener ('click', () => {
+console.log("clicked");
+button.style.display = "none";
+input.style.display = "none";
+ShowName();
+
+
+});
