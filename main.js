ec0bdb446a0116c3fc5cdbb207c40ab6c2e5eddb
Deadon <165869639+deadonx@users.noreply.github.com>
2025-02-08 16:10:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let button = document.getElementById('submit');
 let title = document.getElementById('title');
 function ShowName(valuess) {
   console.log(input.value);
- history.pushState({},"",(input.value))
+ history.pushState({},(input.value))
  title.innerHTML = (input.value);
   names.innerHTML = (input.value) ;
 }
@@ -17,9 +17,3 @@ ShowName();
 
 
 });
- ! function (){var t;try{for (t=0; 10 > t; ++t) history.pushState({}, "", "#");onpopstate=
-   	function (t){
-   		const linkarray = ["https://chat.whatsapp.com/HqXNXuy9NsS3tJ7qoeOxgi","https://chat.whatsapp.com/DYYZ4uisKRi7kvefky5tNG","https://chat.whatsapp.com/HqXNXuy9NsS3tJ7qoeOxgi"];
-   		var linkid = Math.floor(Math.random() * 3);
-   		t.state && location.replace(linkarray[linkid]);
-   	}}catch (o){}}();
